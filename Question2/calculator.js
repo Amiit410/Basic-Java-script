051f7c0f8a2c632f7a50ee049a1a02c40048be19
amitkumar linge <amitlinge410@gmail.com>
2026-02-11 10:54:45 +0530

    objective 2

---
+++ Question2/calculator.js
@@ -0,0 +1,101 @@
+function ValidateInput(){
+    var IsValidated = false;
+    var value1 = document.getElementById("value1").value;
+    var value2 = document.getElementById("value2").value;
+    var pattern1 = new RegExp(/^[0-9]{0,10}/);
+    var pattern2 = new RegExp(/^[0-9]{0,10}/);
+
+    if(!value1)
+    {
+        document.getElementById("value1").style.border = "2px solid red"
+        IsValidated = false;
+    }
+    else if(!pattern1.test(value1))
+    {
+        document.getElementById("value1").style.border = "2px solid red"
+        IsValidated = false;
+    }
+    else
+    {
+        document.getElementById("value1").style.border = "none"
+        IsValidated = true;
+    }
+    if(!value2)
+    {
+        document.getElementById("value2").style.border = "2px solid red"
+        IsValidated = false;
+    }
+    else if(!pattern2.test(value2))
+    {
+        document.getElementById("value2").style.border = "2px solid red"
+        IsValidated = false;
+    }
+    else
+    {
+        document.getElementById("value2").style.border = "none"
+        IsValidated = true;
+    }
+    return IsValidated;
+}
+
+
+function Addition()
+{
+    var val1 = parseFloat(document.getElementById("value1").value);
+    var val2 = parseFloat(document.getElementById("value2").value);
+
+    if(!ValidateInput()){
+        return;
+    }
+    else
+    {
+        var sum = val1+val2;
+        document.getElementById("result").value = sum;
+    }
+}
+function Substraction()
+{
+    var val1 = parseFloat(document.getElementById("value1").value);
+    var val2 = parseFloat(document.getElementById("value2").value);
+
+    if(!ValidateInput()){
+        return;
+    }
+    else
+    {
+        var sum = val1-val2;
+        document.getElementById("result").value = sum;
+    }
+}
+function Multiplication()
+{
+    var val1 = parseFloat(document.getElementById("value1").value);
+    var val2 = parseFloat(document.getElementById("value2").value);
+
+    if(!ValidateInput()){
+        return;
+    }
+    else
+    {
+        var sum = val1*val2;
+        document.getElementById("result").value = sum;
+    }
+}
+function Division()
+{
+    var val1 = parseFloat(document.getElementById("value1").value);
+    var val2 = parseFloat(document.getElementById("value2").value);
+    if(val2 == 0)
+    {
+        document.getElementById("value2").style.border = "2px solid red";
+        return;
+    }
+    if(!ValidateInput()){
+        return;
+    }
+    else
+    {
+        var sum = val1/val2;
+        document.getElementById("result").value = sum;
+    }
+}
