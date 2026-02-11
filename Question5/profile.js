a106a684317d768de1b8bee83a99d1763f598301
amitkumar linge <amitlinge410@gmail.com>
2026-02-11 17:16:54 +0530

    objective 6

---
+++ Question5/profile.js
@@ -0,0 +1,11 @@
+function linkedin(){
+    window.open("https://www.linkedin.com/in/amitkumar-s-l-397432143/","_blank")
+}
+
+function github(){
+    window.open("https://github.com/Amiit410","_blank");
+}
+
+function instagram(){
+    window.open("https://www.instagram.com/amiit4_10/","_blank");
+}
