364925cd8fc741cc2c99f2e0cddeb65e2651f56b
amitkumar linge <amitlinge410@gmail.com>
2026-02-17 22:14:47 +0530

    fullscreen option

--- assignment jQuery/Question3/ToDoList.js
+++ assignment jQuery/Question3/ToDoList.js
@@ -1,9 +1,8 @@
 $(function () {
-    $("#addtask").click(function () {
+    function addtask() {
         var tablelength = $("#table2 tr").length;
 
         var task = $("#input").val();
-
 
         if (!task) { return; }
 
@@ -14,23 +13,20 @@ $(function () {
         td1.textContent = tablelength + 1;
 
         var td2 = document.createElement("td");
-        td2.id = `task${tablelength+1}`;
+        td2.id = `task${tablelength + 1}`;
         td2.textContent = task;
 
         var td3 = document.createElement("td");
         var checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.id = `checkbox${tablelength+1}`;
-
+        checkbox.id = `checkbox${tablelength + 1}`;
         checkbox.addEventListener("click", function () {
             var ischecked = $(`#${checkbox.id}`).is(":checked");
-            if(ischecked)
-            {
-                $(`#${td2.id}`).css("text-decoration","line-through");
+            if (ischecked) {
+                $(`#${td2.id}`).css("text-decoration", "line-through");
             }
-            else
-            {
-                $(`#${td2.id}`).css("text-decoration","none");
+            else {
+                $(`#${td2.id}`).css("text-decoration", "none");
             }
         })
 
@@ -56,5 +52,24 @@ $(function () {
         table2.appendChild(tr);
 
         $("#input").val("");
+    }
+
+    $("#input").keyup(function (e) {
+        if (e.key === "Enter") {
+            addtask();
+        }
     })
+    $("body").keyup(function (e) {
+        if (e.key === "f") {
+            if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen();
+            }
+            else {
+                document.exitFullscreen();
+            }
+        }
+    })
+
+
+    $("#addtask").click(addtask)
 })
